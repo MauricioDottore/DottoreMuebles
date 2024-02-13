efb46e0fd8633629509f7fd1772076d9da292c7c
Mauricio Dottore <mauriciodottore@gmail.com>
2024-02-13 17:41:56 -0300

    Changes

--- script.js
+++ script.js
@@ -86,3 +86,23 @@ document.querySelector('.burger-menu').addEventListener('click', function() {
 });
 
 /*Obras realizadas*/
+/*Servicios*/
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".servicios .nav-menu a");
+
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
